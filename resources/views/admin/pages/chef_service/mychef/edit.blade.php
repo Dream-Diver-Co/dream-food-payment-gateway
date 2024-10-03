65f544543876ed43c5c5e880e0101eb242685b2e
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-10-03 14:00:35 +0600

    03

--- resources/views/admin/pages/chef_service/mychef/edit.blade.php
+++ resources/views/admin/pages/chef_service/mychef/edit.blade.php
@@ -1,18 +1,19 @@
 @extends('admin.layouts.master')
 
-@section('title', 'Profile')
+@section('title', 'Edit Mychef')
+
 @section('content')
 
 <!-- start page title -->
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-            <h4 class="mb-sm-0">Profile</h4>
+            <h4 class="mb-sm-0">Edit Mychef</h4>
 
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">{{ env('APP_NAME') }}</a></li>
-                    <li class="breadcrumb-item active">Profile</li>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Edit Mychef</li>
                 </ol>
             </div>
 
@@ -20,33 +21,24 @@
     </div>
 </div>
 <!-- end page title -->
-
 <div class="card">
-  <div class="card-header">Edit Mychef</div>
-  <div class="card-body">
-
-    <form action="{{ route('mychef.update', $mychef->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" name="name" value="{{ $mychef->name }}" required>
-        </div>
-        <div>
-            <label for="description">Description:</label>
-            <textarea name="description" required>{{ $mychef->description }}</textarea>
-        </div>
-        <div>
-            <label for="image">Image:</label>
-            <input type="file" name="image">
+    <div class="card-header">Edit Mychef</div>
+    <div class="card-body">
+        <form action="{{ route('mychef.update', $mychef->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <label for="name">Name</label><br>
+            <input type="text" name="name" id="name" value="{{ $mychef->name }}" class="form-control"><br>
+            <label for="description">Description</label><br>
+            <textarea name="description" id="description" class="form-control">{{ $mychef->description }}</textarea><br>
+            <label for="image">Image</label><br>
+            <input type="file" name="image" id="image" class="form-control"><br>
             <p>Current Image: <img src="{{ asset('storage/'.$mychef->image) }}" alt="{{ $mychef->name }}" width="100"></p>
-        </div>
-        <button type="submit">Update</button>
-    </form>
-
-  </div>
+           <button type="submit" class="btn btn-success">Update</button><br>
+        </form>
+    </div>
 </div>
 
-
 @endsection
+
 
