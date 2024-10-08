855fb1b88aa946f0aed550f07e4bfafd93fe8bac
Sazal-Abdullah <sazalahamed61@gmail.com>
2024-10-08 11:12:15 +0600

    08

--- resources/views/frontend/page/stripe.blade.php
+++ resources/views/frontend/page/stripe.blade.php
@@ -127,7 +127,6 @@
                             </div>
                         </div>
                     </div> --}}
-                    
                     <div class="mb-5">
                         <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Payment</span></h5>
                         <div class="bg-wheat p-4">
